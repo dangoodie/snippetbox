4cf87b5b1409aa3fdc7ee10fe8dab816ec9f538d
dangoodie <dan.gooden.dev@gmail.com>
2025-09-26 21:51:19 +1000

    Completing chapter 4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/dangoodie/snippetbox/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
-
+	_ = godotenv.Load()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	dbPass, ok := os.LookupEnv("MYSQL_PASSWORD")
+	if !ok {
+		logger.Error("MYSQL_PASSWORD environment variable is not set")
+		os.Exit(1)
+	}
+
+	defDsn := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", dbPass)
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", defDsn, "MySQL data source name")
+	flag.Parse()
+
+	db, err := openDb(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("Starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }

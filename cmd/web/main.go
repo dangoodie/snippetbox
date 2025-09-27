e7b8e2082248dba167c7b08add4d7366d481a9af
dangoodie <dan.gooden.dev@gmail.com>
2025-09-27 11:55:50 +1000

    Completing chapter 8

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,42 +2,48 @@ package main
 
 import (
 	"database/sql"
-	"flag"
+	"errors"
 	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
+
+	flag "github.com/spf13/pflag"
 
 	"github.com/dangoodie/snippetbox/internal/models"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 type application struct {
-	logger        *slog.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
 	_ = godotenv.Load()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	dbPass, ok := os.LookupEnv("MYSQL_PASSWORD")
-	if !ok {
-		logger.Error("MYSQL_PASSWORD environment variable is not set")
-		os.Exit(1)
-	}
-
-	defDsn := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", dbPass)
-
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", defDsn, "MySQL data source name")
+	dsn := flag.String("dsn", "", "MySQL data source name")
 	flag.Parse()
+
+	if *dsn == "" {
+		err := setDefaultDSN(dsn)
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+	}
 
 	db, err := openDb(*dsn)
 	if err != nil {
@@ -54,11 +60,16 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	sessionManger := scs.New()
+	sessionManger.Store = mysqlstore.New(db)
+	sessionManger.Lifetime = 12 * time.Hour
+
 	app := &application{
-		logger:        logger,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManger,
 	}
 
 	logger.Info("Starting server", "addr", *addr)
@@ -80,4 +91,14 @@ func openDb(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func setDefaultDSN(dsn *string) error {
+	dbPass, ok := os.LookupEnv("MYSQL_PASSWORD")
+	if !ok {
+		return errors.New("MYSQL_PASSWORD environment variable is not set")
+
+	}
+	*dsn = fmt.Sprintf("web:%s@/snippetbox?parseTime=true", dbPass)
+	return nil
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"html/template"
-	"net/http"
 	"path/filepath"
 	"time"
 
@@ -14,6 +13,7 @@ type templateData struct {
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
 	Form        any
+	Flash       string
 }
 
 var functions = template.FuncMap{
@@ -48,12 +48,6 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	return cache, nil
-}
-
-func (app *application) newTemplateData(r *http.Request) templateData {
-	return templateData{
-		CurrentYear: time.Now().Year(),
-	}
 }
 
 func humanDate(t time.Time) string {

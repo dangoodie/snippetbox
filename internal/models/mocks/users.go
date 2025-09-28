d3f84208f3e52ba0ec95f8f6cd34ae84290dbbc3
dangoodie <dan.gooden.dev@gmail.com>
2025-09-29 00:12:11 +1000

    Adding account page

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -1,6 +1,8 @@
 package mocks
 
 import (
+	"time"
+
 	"github.com/dangoodie/snippetbox/internal/models"
 )
 
@@ -29,5 +31,20 @@ func (m *UserModel) Exists(id int) (bool, error) {
 		return true, nil
 	default:
 		return false, nil
+	}
+}
+
+func (m *UserModel) Get(id int) (models.User, error) {
+	switch id {
+	case 1:
+		return models.User{
+			ID:             1,
+			Name:           "Alice Example",
+			Email:          "alice@example.com",
+			HashedPassword: []byte(""),
+			Created:        time.Now(),
+		}, nil
+	default:
+		return models.User{}, models.ErrNoRecord
 	}
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -14,6 +14,7 @@ type UserModelInterface interface {
 	Insert(name, email, password string) error
 	Authenticate(email, password string) (int, error)
 	Exists(id int) (bool, error)
+	Get(id int) (User, error)
 }
 
 type User struct {
@@ -88,4 +89,24 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	}
 
 	return exists, nil
+}
+
+func (m *UserModel) Get(id int) (User, error) {
+	stmt := `SELECT id, name, email, created FROM users
+	WHERE id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	var u User
+
+	err := row.Scan(&u.ID, &u.Name, &u.Email, &u.Created)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return User{}, ErrNoRecord
+		} else {
+			return User{}, err
+		}
+	}
+
+	return u, nil
 }
